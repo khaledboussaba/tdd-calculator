32956146efa7463ff69fb979db6ea00490421f3e
khaled <khaledboussaba@gmail.com>
2020-04-09 15:52:49 +0200

    add some parameterized test uning annotations

--- src/test/java/fr/tdd/calculator/CalculatorTest.java
+++ src/test/java/fr/tdd/calculator/CalculatorTest.java
@@ -3,6 +3,9 @@ package fr.tdd.calculator;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.text.MessageFormat;
 import java.time.Duration;
@@ -41,13 +44,27 @@ public class CalculatorTest {
     }
 
     @Test
-    public void addTwoPositiveNumbers() {
+    public void addition_addTwoPositiveNumbers() {
         assertEquals(42, calculatorUnderTest.add(40, 2));
     }
 
+    @ParameterizedTest(name = "{0} + {1} should equal to {2}")
+    @CsvSource({"1,1,2", "4,5,9", "99,122,221"})
+    public void addition_ShouldReturnTheSumOfMultipleIntegers(int arg1, int arg2, int expectResult) {
+        int actualResult = calculatorUnderTest.add(arg1,arg2);
+        assertEquals(expectResult, actualResult);
+    }
+
     @Test
-    public void multiplyTwoPosistiveNumbers() {
+    public void multiplication_multiplyTwoPosistiveNumbers() {
         assertEquals(35, calculatorUnderTest.multiply(5, 7));
+    }
+
+    @ParameterizedTest(name = "{0} x 0 should equal to 0")
+    @ValueSource(ints = {1, 2, 42, 10011, 5988})
+    public void multiplication_MultiplyOfZeroWithMultipleIntegersShouldReturnZero(int arg) {
+        int actualResult = calculatorUnderTest.multiply(arg, 0);
+        assertEquals(0, actualResult);
     }
 
 }
