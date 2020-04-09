00ebe645d15d57334ba0c38db0d0354a9133630b
khaled <khaledboussaba@gmail.com>
2020-04-09 15:31:47 +0200

    add some annotations using JUnit 5 like beforeAll & afterAll

--- src/test/java/fr/tdd/calculator/CalculatorTest.java
+++ src/test/java/fr/tdd/calculator/CalculatorTest.java
@@ -2,20 +2,52 @@ package fr.tdd.calculator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.text.MessageFormat;
+import java.time.Duration;
+import java.time.Instant;
 
 public class CalculatorTest {
 
-    @Test
-    void addTwoPositiveNumbers() {
-        Calculator calculator = new Calculator();
-        assertEquals(42, calculator.add(40, 2));
+    private static Instant startedAt;
+
+    private Calculator calculatorUnderTest;
+
+    @BeforeAll
+    public static void initStartingTime() {
+        System.out.println("call before all tests");
+        startedAt = Instant.now();
+    }
+
+    @AfterAll
+    public static void showTestsDuration() {
+        System.out.println("call after all tests");
+        Instant endedAt = Instant.now();
+        long duration = Duration.between(startedAt, endedAt).toMillis();
+        System.out.println(MessageFormat.format("Tests duration is : {0} ms", duration));
+    }
+
+    @BeforeEach
+    public void initCalculator() {
+        System.out.println("call before each test");
+        calculatorUnderTest = new Calculator();
+    }
+
+    @AfterEach
+    public void afterEachTest() {
+        System.out.println("call after each test");
+        calculatorUnderTest = null;
     }
 
     @Test
-    void multiplyTwoPosistiveNumbers() {
-        Calculator calculator = new Calculator();
-        assertEquals(35, calculator.multiply(5, 7));
+    public void addTwoPositiveNumbers() {
+        assertEquals(42, calculatorUnderTest.add(40, 2));
+    }
+
+    @Test
+    public void multiplyTwoPosistiveNumbers() {
+        assertEquals(35, calculatorUnderTest.multiply(5, 7));
     }
 
 }
