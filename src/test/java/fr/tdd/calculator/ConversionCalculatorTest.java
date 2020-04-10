f13eeab9b3183cbeaa3f48ef5edff33c66663373
khaled <khaledboussaba@gmail.com>
2020-04-10 15:04:57 +0200

    group tests for temperature in an internal class using Nested annotation

--- src/test/java/fr/tdd/calculator/ConversionCalculatorTest.java
+++ src/test/java/fr/tdd/calculator/ConversionCalculatorTest.java
@@ -13,36 +13,44 @@ import java.util.Set;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.withinPercentage;
 
+@Tag("ConversionTests")
+@DisplayName("successfully convert between different units")
 public class ConversionCalculatorTest {
 
     private CoversionCalculator calculatorUnderTest = new CoversionCalculator();
 
-    @Test
-    @DisplayName("we have a temperature in celsius equal to 0°C, when we convert it to fahrenheit, we should get 32°F")
-    public void celsiusToFahrenheit_returnsAFahrenheitTempurature_whenCelsiusIsZero() {
-        Double actualFahrenheit = calculatorUnderTest.celsiusToFahrenheit(0.0);
-        assertThat(actualFahrenheit).isCloseTo(32.0, withinPercentage(0.01));
-    }
+    @Nested
+    @Tag("TemperaturesTests")
+    @DisplayName("successfully convert temperatures")
+    class TemperatureTest {
+        @Test
+        @DisplayName("we have a temperature in celsius equal to 0°C, when we convert it to fahrenheit, we should get 32°F")
+        public void celsiusToFahrenheit_returnsAFahrenheitTempurature_whenCelsiusIsZero() {
+            Double actualFahrenheit = calculatorUnderTest.celsiusToFahrenheit(0.0);
+            assertThat(actualFahrenheit).isCloseTo(32.0, withinPercentage(0.01));
+        }
 
-    @Test
-    @DisplayName("we have a temperature in celsius equal to 32°C, when we convert it to fahrenheit, we should get 89.6°F")
-    public void celsiusToFahrenheit_returnsAFahrenheitTempurature_whenCelsiusIsThirtyTwo() {
-        Double actualFahrenheit = calculatorUnderTest.celsiusToFahrenheit(32.0);
-        assertThat(actualFahrenheit).isCloseTo(89.6, withinPercentage(0.01));
-    }
+        @Test
+        @DisplayName("we have a temperature in celsius equal to 32°C, when we convert it to fahrenheit, we should get 89.6°F")
+        public void celsiusToFahrenheit_returnsAFahrenheitTempurature_whenCelsiusIsThirtyTwo() {
+            Double actualFahrenheit = calculatorUnderTest.celsiusToFahrenheit(32.0);
+            assertThat(actualFahrenheit).isCloseTo(89.6, withinPercentage(0.01));
+        }
 
-    @Test
-    @DisplayName("we have a temperature in fahrenheit equal to 0°F, when we convert it to celsius, we should get -17.77°C")
-    public void fahrenheitToCelsius_returnsACelsiusTempurature_whenFahrenheitIsZero() {
-        Double actualCelsius = calculatorUnderTest.fahrenheitToCelsius(0.0);
-        assertThat(actualCelsius).isCloseTo(-17.77, withinPercentage(0.05));
-    }
+        @Test
+        @DisplayName("we have a temperature in fahrenheit equal to 0°F, when we convert it to celsius, we should get -17.77°C")
+        public void fahrenheitToCelsius_returnsACelsiusTempurature_whenFahrenheitIsZero() {
+            Double actualCelsius = calculatorUnderTest.fahrenheitToCelsius(0.0);
+            assertThat(actualCelsius).isCloseTo(-17.77, withinPercentage(0.05));
+        }
 
-    @Test
-    @DisplayName("we have a temperature in fahrenheit equal to 32°F, when we convert it to celsius, we should get 0.0°C")
-    public void fahrenheitToCelsius_returnsACelsiusTempurature_whenFahrenheitIsThirtyTwo() {
-        Double actualCelsius = calculatorUnderTest.fahrenheitToCelsius(32.0);
-        assertThat(actualCelsius).isCloseTo(0.0, withinPercentage(0.01));
+        @Test
+        @DisplayName("we have a temperature in fahrenheit equal to 32°F, when we convert it to celsius, we should get 0.0°C")
+        public void fahrenheitToCelsius_returnsACelsiusTempurature_whenFahrenheitIsThirtyTwo() {
+            Double actualCelsius = calculatorUnderTest.fahrenheitToCelsius(32.0);
+            assertThat(actualCelsius).isCloseTo(0.0, withinPercentage(0.01));
+        }
+
     }
 
 }
